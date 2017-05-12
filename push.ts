f76edb341dba88d0885711bf4e2e8093a37c3d4a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-05-12 10:49:00 -0700

    wip: switch to VAPID

--- push.ts
+++ push.ts
@@ -1,40 +1,21 @@
-import * as fs from 'fs';
+const push = require('web-push');
 
-declare interface WebPushParams {
-  TTL?: number;
-  userPublicKey?: Buffer;
-  userAuth?: Buffer;
-  payload?: any;
-  vapid?: any;
-}
-
-declare interface WebPush {
-  sendNotification(endpoint: string, params: WebPushParams): Promise<any>;
-  setGCMAPIKey(key: string);
-}
-
-let push: WebPush = require('web-push');
-
-export function sendPush(reg: any, payload?: Object): Promise<any> {
-  let endpoint = reg.url;
-  return push.sendNotification(endpoint, {
-    userPublicKey: reg.key,
-    userAuth: reg.auth,
-    payload: new Buffer(JSON.stringify(payload))
-  }).catch(err => console.error(err));
-}
-
-const key = JSON.parse(fs.readFileSync('./.ngsw-config.json').toString())['gcm_key'];
-push.setGCMAPIKey(key);
-
-const reg = JSON.parse(process.argv[2]);
-const msg = process.argv[3];
-
-sendPush(reg, {
+const subscription = JSON.parse(process.argv[2]);
+const payload = {
   notification: {
-    title: 'Demo Push',
-    body: msg,
-    icon: '/assets/logo.png',
-    requireInteration: false,
+    title: 'NgStore',
+    icon: '/assets/logo-96.png',
+    body: 'Angular PWAs at Google IO!',
+    requireInteraction: false,
   },
-})
+};
+
+const credentials = {
+  vapidDetails: {
+    subject: 'mailto:test@angular.io',
+    publicKey: 'BPm3UsR79ZIPppvRYEkWpDzbPB8tTSKKsG16qmvCPv6tyBdUMUu8lOZoRxtLnVSkTbzQlp0b6oJ_1TYXt3sMFdo',
+    privateKey: '8KHDAvQF77xinlev5DhLXhz0yzWVRbNcrJuvPhDzcV8',
+  },
+};
+
+push.sendNotification(subscription, payload, credentials).catch(err => console.error(err));
