b7068238d230417b09398b76e87ed5927fbb6ae7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-05-12 13:05:52 -0700

    wip: reset to latest state

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
 PATH=$PATH:$(npm bin)
 set -x
 
-# Production build
+# Initial clean
 rm -rf dist/*
+
+# Production build
 ng build --prod
+
+# Copy the web app manifest to the hosting root.
 cp src/manifest.json dist/
 
 # Serve
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  constructor() {}
+  constructor() {
+  }
 }
