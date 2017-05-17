7f670676a4c0e2d7ad1688519acf10db175d526a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-05-17 09:31:21 -0700

    wip: fixup run.sh

--- run.sh
+++ run.sh
@@ -2,14 +2,8 @@
 PATH=$PATH:$(npm bin)
 set -x
 
-# Initial clean
-rm -rf dist/*
-
 # Production build
 ng build --prod
-
-# Copy the web app manifest to the hosting root.
-cp src/manifest.json dist/
 
 # Serve
 cd dist
